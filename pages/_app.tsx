2ddc3220007f2261a346888bea8b62f7868c19da
Frederico Guilherme Klüser de Oliveira <kluserhuu@gmail.com>
2024-05-16 09:40:56 -0300

    feat: Add user audio playback functionality

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import { Analytics } from "@vercel/analytics/react";
 import Header from "../components/header";
 import { useEffect, useState } from "react";
 import { DarkContext } from "../utils/darkContext";
+import useLocalStorage from "@/hooks/useLocalStorage";
 // import * as Sentry from "@sentry/nextjs";
 
 // GO: https://sentry.io/
@@ -16,6 +17,12 @@ const DARK_MODE = "DARK";
 
 export default function App({ Component, pageProps }: AppProps) {
   const [dark, setDark] = useState(false);
+  const user = useLocalStorage("user");
+  const [currentAudio, setCurrentAudio] = useState<HTMLAudioElement | null>(
+    null
+  );
+
+  const isMyLove = () => user === "claudia";
 
   useEffect(() => {
     const darkMode = localStorage.getItem(DARK_MODE);
@@ -28,11 +35,34 @@ export default function App({ Component, pageProps }: AppProps) {
     localStorage.setItem(DARK_MODE, dark ? "dark" : "");
   }, [dark]);
 
+  const playAudio = (path: string) => {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0; // Reseta o tempo do áudio anterior
+    }
+
+    const newAudio = new Audio(path);
+    newAudio.play();
+    setCurrentAudio(newAudio);
+  };
+
+  useEffect(() => {
+    if (isMyLove()) {
+      playAudio(
+        "/John Paul Young - Love Is In The Air (John Junior & Oscar Edit).mp3"
+      );
+    }
+  }, [user]);
+
   return (
     <>
       <div className={`${dark ? "dark" : ""}`}>
         <DarkContext.Provider value={dark}>
-          <div className="min-h-screen text-slate-900 dark:bg-slate-900 dark:text-slate-100">
+          <div
+            className={`${
+              isMyLove() ? "love-background" : ""
+            } min-h-screen text-slate-900 dark:bg-slate-900 dark:text-slate-100`}
+          >
             <Header dark={dark} setDark={setDark} />
             <main className="mx-auto flex flex-col items-center">
               <Component {...pageProps} />
